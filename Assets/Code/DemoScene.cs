27c2b43b67070ce138136f358861aff429b1f362
Kev Danaher <danaherk@outlook.com>
2025-03-22 15:42:27 -0300

    Removing superfluous scene and adding missing audio data

--- Assets/Code/DemoScene.cs
+++ Assets/Code/DemoScene.cs
@@ -27,12 +27,6 @@ public class DemoScene : MonoBehaviour
     [SerializeField]
     private Slider _musicVolumeSlider;
 
-    [SerializeField]
-    private Button _nextSceneButton;
-
-    [SerializeField]
-    private string _nextSceneName;
-
     private int _currentMusicTrack = 0;
 
     void Start()
@@ -41,7 +35,6 @@ public class DemoScene : MonoBehaviour
         _stopLoopingSoundButton.onClick.AddListener(StopLoopingSound);
         _switchMusicTrackButton.onClick.AddListener(SwitchMusicTrack);
         _crossfadeMusicButton.onClick.AddListener(CrossfadeMusic);
-        _nextSceneButton.onClick.AddListener(LoadNextScene);
         _soundVolumeSlider.onValueChanged.AddListener(SetSoundVolume);
         _musicVolumeSlider.onValueChanged.AddListener(SetMusicVolume);
 
@@ -99,16 +92,5 @@ public class DemoScene : MonoBehaviour
     public void SetSoundVolume(float volume)
     {
         AudioManager.Instance.SetSoundVolume(volume);
-    }
-
-    private void LoadNextScene()
-    {
-        if (_nextSceneName != null)
-        {
-            SceneManager.LoadSceneAsync(_nextSceneName).completed += (AsyncOperation op) =>
-            {
-                SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
-            };
-        }
     }
 }
